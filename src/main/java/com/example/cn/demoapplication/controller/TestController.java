faf61d0b59a64382035c7268729abacb2ed27eb2
huyaou <1009003540@qq.com>
2019-10-14 16:50:05 +0800

    取消之前的测试内容

--- src/main/java/com/example/cn/demoapplication/controller/TestController.java
+++ src/main/java/com/example/cn/demoapplication/controller/TestController.java
@@ -13,8 +13,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    //随意 ceshi
-    public void say() {
-        System.out.println("这是测试...");
-    }
 }
